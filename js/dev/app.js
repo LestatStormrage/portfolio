74b676c5ab11d8c83f70d9a837c3ec2540722f0a
LestatStormrage <dinarin.ivan@gmail.com>
2026-04-24 02:59:00 +0200

    Updates

--- js/dev/app.js
+++ js/dev/app.js
@@ -631,122 +631,6 @@ dropdownBtn.addEventListener("click", (e) => {
 document.addEventListener("click", () => {
   dropdown.classList.remove("open");
 });
-function tabs() {
-  const tabs2 = document.querySelectorAll("[data-fls-tabs]");
-  let tabsActiveHash = [];
-  if (tabs2.length > 0) {
-    const hash = getHash();
-    if (hash && hash.startsWith("tab-")) {
-      tabsActiveHash = hash.replace("tab-", "").split("-");
-    }
-    tabs2.forEach((tabsBlock, index) => {
-      tabsBlock.classList.add("--tab-init");
-      tabsBlock.setAttribute("data-fls-tabs-index", index);
-      tabsBlock.addEventListener("click", setTabsAction);
-      initTabs(tabsBlock);
-    });
-    let mdQueriesArray = dataMediaQueries(tabs2, "flsTabs");
-    if (mdQueriesArray && mdQueriesArray.length) {
-      mdQueriesArray.forEach((mdQueriesItem) => {
-        mdQueriesItem.matchMedia.addEventListener("change", function() {
-          setTitlePosition(mdQueriesItem.itemsArray, mdQueriesItem.matchMedia);
-        });
-        setTitlePosition(mdQueriesItem.itemsArray, mdQueriesItem.matchMedia);
-      });
-    }
-  }
-  function setTitlePosition(tabsMediaArray, matchMedia) {
-    tabsMediaArray.forEach((tabsMediaItem) => {
-      tabsMediaItem = tabsMediaItem.item;
-      let tabsTitles = tabsMediaItem.querySelector("[data-fls-tabs-titles]");
-      let tabsTitleItems = tabsMediaItem.querySelectorAll("[data-fls-tabs-title]");
-      let tabsContent = tabsMediaItem.querySelector("[data-fls-tabs-body]");
-      let tabsContentItems = tabsMediaItem.querySelectorAll("[data-fls-tabs-item]");
-      tabsTitleItems = Array.from(tabsTitleItems).filter((item) => item.closest("[data-fls-tabs]") === tabsMediaItem);
-      tabsContentItems = Array.from(tabsContentItems).filter((item) => item.closest("[data-fls-tabs]") === tabsMediaItem);
-      tabsContentItems.forEach((tabsContentItem, index) => {
-        if (matchMedia.matches) {
-          tabsContent.append(tabsTitleItems[index]);
-          tabsContent.append(tabsContentItem);
-          tabsMediaItem.classList.add("--tab-spoller");
-        } else {
-          tabsTitles.append(tabsTitleItems[index]);
-          tabsMediaItem.classList.remove("--tab-spoller");
-        }
-      });
-    });
-  }
-  function initTabs(tabsBlock) {
-    let tabsTitles = tabsBlock.querySelectorAll("[data-fls-tabs-titles]>*");
-    let tabsContent = tabsBlock.querySelectorAll("[data-fls-tabs-body]>*");
-    const tabsBlockIndex = tabsBlock.dataset.flsTabsIndex;
-    const tabsActiveHashBlock = tabsActiveHash[0] == tabsBlockIndex;
-    if (tabsActiveHashBlock) {
-      const tabsActiveTitle = tabsBlock.querySelector("[data-fls-tabs-titles]>.--tab-active");
-      tabsActiveTitle ? tabsActiveTitle.classList.remove("--tab-active") : null;
-    }
-    if (tabsContent.length) {
-      tabsContent.forEach((tabsContentItem, index) => {
-        tabsTitles[index].setAttribute("data-fls-tabs-title", "");
-        tabsContentItem.setAttribute("data-fls-tabs-item", "");
-        if (tabsActiveHashBlock && index == tabsActiveHash[1]) {
-          tabsTitles[index].classList.add("--tab-active");
-        }
-        tabsContentItem.hidden = !tabsTitles[index].classList.contains("--tab-active");
-      });
-    }
-  }
-  function setTabsStatus(tabsBlock) {
-    let tabsTitles = tabsBlock.querySelectorAll("[data-fls-tabs-title]");
-    let tabsContent = tabsBlock.querySelectorAll("[data-fls-tabs-item]");
-    const tabsBlockIndex = tabsBlock.dataset.flsTabsIndex;
-    function isTabsAnamate(tabsBlock2) {
-      if (tabsBlock2.hasAttribute("data-fls-tabs-animate")) {
-        return tabsBlock2.dataset.flsTabsAnimate > 0 ? Number(tabsBlock2.dataset.flsTabsAnimate) : 500;
-      }
-    }
-    const tabsBlockAnimate = isTabsAnamate(tabsBlock);
-    if (tabsContent.length > 0) {
-      const isHash = tabsBlock.hasAttribute("data-fls-tabs-hash");
-      tabsContent = Array.from(tabsContent).filter((item) => item.closest("[data-fls-tabs]") === tabsBlock);
-      tabsTitles = Array.from(tabsTitles).filter((item) => item.closest("[data-fls-tabs]") === tabsBlock);
-      tabsContent.forEach((tabsContentItem, index) => {
-        if (tabsTitles[index].classList.contains("--tab-active")) {
-          if (tabsBlockAnimate) {
-            slideDown(tabsContentItem, tabsBlockAnimate);
-          } else {
-            tabsContentItem.hidden = false;
-          }
-          if (isHash && !tabsContentItem.closest(".popup")) {
-            setHash(`tab-${tabsBlockIndex}-${index}`);
-          }
-        } else {
-          if (tabsBlockAnimate) {
-            slideUp(tabsContentItem, tabsBlockAnimate);
-          } else {
-            tabsContentItem.hidden = true;
-          }
-        }
-      });
-    }
-  }
-  function setTabsAction(e) {
-    const el = e.target;
-    if (el.closest("[data-fls-tabs-title]")) {
-      const tabTitle = el.closest("[data-fls-tabs-title]");
-      const tabsBlock = tabTitle.closest("[data-fls-tabs]");
-      if (!tabTitle.classList.contains("--tab-active") && !tabsBlock.querySelector(".--slide")) {
-        let tabActiveTitle = tabsBlock.querySelectorAll("[data-fls-tabs-title].--tab-active");
-        tabActiveTitle.length ? tabActiveTitle = Array.from(tabActiveTitle).filter((item) => item.closest("[data-fls-tabs]") === tabsBlock) : null;
-        tabActiveTitle.length ? tabActiveTitle[0].classList.remove("--tab-active") : null;
-        tabTitle.classList.add("--tab-active");
-        setTabsStatus(tabsBlock);
-      }
-      e.preventDefault();
-    }
-  }
-}
-window.addEventListener("load", tabs);
 const fireflyGroups = [
   {
     selector: ".firefly--1",
@@ -871,6 +755,218 @@ function colorShift(el, group) {
 function rand(min, max) {
   return Math.random() * (max - min) + min;
 }
+class Parallax {
+  constructor(elements) {
+    if (elements.length) {
+      this.elements = Array.from(elements).map((el) => new Parallax.Each(el, this.options));
+    }
+  }
+  destroyEvents() {
+    this.elements.forEach((el) => {
+      el.destroyEvents();
+    });
+  }
+  setEvents() {
+    this.elements.forEach((el) => {
+      el.setEvents();
+    });
+  }
+}
+Parallax.Each = class {
+  constructor(parent) {
+    this.parent = parent;
+    this.elements = this.parent.querySelectorAll("[data-fls-parallax]");
+    this.animation = this.animationFrame.bind(this);
+    this.offset = 0;
+    this.value = 0;
+    this.smooth = parent.dataset.flsParallaxSmooth ? Number(parent.dataset.flsParallaxSmooth) : 15;
+    this.setEvents();
+  }
+  setEvents() {
+    this.animationID = window.requestAnimationFrame(this.animation);
+  }
+  destroyEvents() {
+    window.cancelAnimationFrame(this.animationID);
+  }
+  animationFrame() {
+    const topToWindow = this.parent.getBoundingClientRect().top;
+    const heightParent = this.parent.offsetHeight;
+    const heightWindow = document.documentElement.clientHeight;
+    const positionParent = {
+      top: topToWindow - heightWindow,
+      bottom: topToWindow + heightParent
+    };
+    const centerPoint = this.parent.dataset.flsParallaxCenter ? this.parent.dataset.flsParallaxCenter : "center";
+    if (positionParent.top < 30 && positionParent.bottom > -30) {
+      switch (centerPoint) {
+        // верхній точці (початок батька стикається верхнього краю екрану)
+        case "top":
+          this.offset = -1 * topToWindow;
+          break;
+        // центрі екрана (середина батька у середині екрана)
+        case "center":
+          this.offset = heightWindow / 2 - (topToWindow + heightParent / 2);
+          break;
+        // Початок: нижня частина екрана = верхня частина батька
+        case "bottom":
+          this.offset = heightWindow - (topToWindow + heightParent);
+          break;
+      }
+    }
+    this.value = this.offset;
+    this.animationID = window.requestAnimationFrame(this.animation);
+    this.elements.forEach((el) => {
+      const parameters = {
+        axis: el.dataset.axis ? el.dataset.axis : "v",
+        direction: el.dataset.flsParallaxDirection ? el.dataset.flsParallaxDirection + "1" : "-1",
+        coefficient: el.dataset.flsParallaxCoefficient ? Number(el.dataset.flsParallaxCoefficient) : 5,
+        min: el.dataset.flsParallaxMin !== void 0 ? Number(el.dataset.flsParallaxMin) : null,
+        max: el.dataset.flsParallaxMax !== void 0 ? Number(el.dataset.flsParallaxMax) : null,
+        additionalProperties: el.dataset.flsParallaxProperties ? el.dataset.flsParallaxProperties : ""
+      };
+      this.parameters(el, parameters);
+    });
+  }
+  parameters(el, parameters) {
+    let translateValue = parameters.direction * (this.value / parameters.coefficient);
+    if (parameters.min !== null) {
+      translateValue = Math.max(parameters.min, translateValue);
+    }
+    if (parameters.max !== null) {
+      translateValue = Math.min(parameters.max, translateValue);
+    }
+    if (parameters.axis === "v") {
+      el.style.transform = `
+			translate3D(0, ${translateValue.toFixed(2)}px, 0)
+			${parameters.additionalProperties}
+		`;
+    } else if (parameters.axis === "h") {
+      el.style.transform = `
+			translate3D(${translateValue.toFixed(2)}px, 0, 0)
+			${parameters.additionalProperties}
+		`;
+    }
+  }
+};
+if (document.querySelector("[data-fls-parallax-parent]")) {
+  new Parallax(document.querySelectorAll("[data-fls-parallax-parent]"));
+}
+function tabs() {
+  const tabs2 = document.querySelectorAll("[data-fls-tabs]");
+  let tabsActiveHash = [];
+  if (tabs2.length > 0) {
+    const hash = getHash();
+    if (hash && hash.startsWith("tab-")) {
+      tabsActiveHash = hash.replace("tab-", "").split("-");
+    }
+    tabs2.forEach((tabsBlock, index) => {
+      tabsBlock.classList.add("--tab-init");
+      tabsBlock.setAttribute("data-fls-tabs-index", index);
+      tabsBlock.addEventListener("click", setTabsAction);
+      initTabs(tabsBlock);
+    });
+    let mdQueriesArray = dataMediaQueries(tabs2, "flsTabs");
+    if (mdQueriesArray && mdQueriesArray.length) {
+      mdQueriesArray.forEach((mdQueriesItem) => {
+        mdQueriesItem.matchMedia.addEventListener("change", function() {
+          setTitlePosition(mdQueriesItem.itemsArray, mdQueriesItem.matchMedia);
+        });
+        setTitlePosition(mdQueriesItem.itemsArray, mdQueriesItem.matchMedia);
+      });
+    }
+  }
+  function setTitlePosition(tabsMediaArray, matchMedia) {
+    tabsMediaArray.forEach((tabsMediaItem) => {
+      tabsMediaItem = tabsMediaItem.item;
+      let tabsTitles = tabsMediaItem.querySelector("[data-fls-tabs-titles]");
+      let tabsTitleItems = tabsMediaItem.querySelectorAll("[data-fls-tabs-title]");
+      let tabsContent = tabsMediaItem.querySelector("[data-fls-tabs-body]");
+      let tabsContentItems = tabsMediaItem.querySelectorAll("[data-fls-tabs-item]");
+      tabsTitleItems = Array.from(tabsTitleItems).filter((item) => item.closest("[data-fls-tabs]") === tabsMediaItem);
+      tabsContentItems = Array.from(tabsContentItems).filter((item) => item.closest("[data-fls-tabs]") === tabsMediaItem);
+      tabsContentItems.forEach((tabsContentItem, index) => {
+        if (matchMedia.matches) {
+          tabsContent.append(tabsTitleItems[index]);
+          tabsContent.append(tabsContentItem);
+          tabsMediaItem.classList.add("--tab-spoller");
+        } else {
+          tabsTitles.append(tabsTitleItems[index]);
+          tabsMediaItem.classList.remove("--tab-spoller");
+        }
+      });
+    });
+  }
+  function initTabs(tabsBlock) {
+    let tabsTitles = tabsBlock.querySelectorAll("[data-fls-tabs-titles]>*");
+    let tabsContent = tabsBlock.querySelectorAll("[data-fls-tabs-body]>*");
+    const tabsBlockIndex = tabsBlock.dataset.flsTabsIndex;
+    const tabsActiveHashBlock = tabsActiveHash[0] == tabsBlockIndex;
+    if (tabsActiveHashBlock) {
+      const tabsActiveTitle = tabsBlock.querySelector("[data-fls-tabs-titles]>.--tab-active");
+      tabsActiveTitle ? tabsActiveTitle.classList.remove("--tab-active") : null;
+    }
+    if (tabsContent.length) {
+      tabsContent.forEach((tabsContentItem, index) => {
+        tabsTitles[index].setAttribute("data-fls-tabs-title", "");
+        tabsContentItem.setAttribute("data-fls-tabs-item", "");
+        if (tabsActiveHashBlock && index == tabsActiveHash[1]) {
+          tabsTitles[index].classList.add("--tab-active");
+        }
+        tabsContentItem.hidden = !tabsTitles[index].classList.contains("--tab-active");
+      });
+    }
+  }
+  function setTabsStatus(tabsBlock) {
+    let tabsTitles = tabsBlock.querySelectorAll("[data-fls-tabs-title]");
+    let tabsContent = tabsBlock.querySelectorAll("[data-fls-tabs-item]");
+    const tabsBlockIndex = tabsBlock.dataset.flsTabsIndex;
+    function isTabsAnamate(tabsBlock2) {
+      if (tabsBlock2.hasAttribute("data-fls-tabs-animate")) {
+        return tabsBlock2.dataset.flsTabsAnimate > 0 ? Number(tabsBlock2.dataset.flsTabsAnimate) : 500;
+      }
+    }
+    const tabsBlockAnimate = isTabsAnamate(tabsBlock);
+    if (tabsContent.length > 0) {
+      const isHash = tabsBlock.hasAttribute("data-fls-tabs-hash");
+      tabsContent = Array.from(tabsContent).filter((item) => item.closest("[data-fls-tabs]") === tabsBlock);
+      tabsTitles = Array.from(tabsTitles).filter((item) => item.closest("[data-fls-tabs]") === tabsBlock);
+      tabsContent.forEach((tabsContentItem, index) => {
+        if (tabsTitles[index].classList.contains("--tab-active")) {
+          if (tabsBlockAnimate) {
+            slideDown(tabsContentItem, tabsBlockAnimate);
+          } else {
+            tabsContentItem.hidden = false;
+          }
+          if (isHash && !tabsContentItem.closest(".popup")) {
+            setHash(`tab-${tabsBlockIndex}-${index}`);
+          }
+        } else {
+          if (tabsBlockAnimate) {
+            slideUp(tabsContentItem, tabsBlockAnimate);
+          } else {
+            tabsContentItem.hidden = true;
+          }
+        }
+      });
+    }
+  }
+  function setTabsAction(e) {
+    const el = e.target;
+    if (el.closest("[data-fls-tabs-title]")) {
+      const tabTitle = el.closest("[data-fls-tabs-title]");
+      const tabsBlock = tabTitle.closest("[data-fls-tabs]");
+      if (!tabTitle.classList.contains("--tab-active") && !tabsBlock.querySelector(".--slide")) {
+        let tabActiveTitle = tabsBlock.querySelectorAll("[data-fls-tabs-title].--tab-active");
+        tabActiveTitle.length ? tabActiveTitle = Array.from(tabActiveTitle).filter((item) => item.closest("[data-fls-tabs]") === tabsBlock) : null;
+        tabActiveTitle.length ? tabActiveTitle[0].classList.remove("--tab-active") : null;
+        tabTitle.classList.add("--tab-active");
+        setTabsStatus(tabsBlock);
+      }
+      e.preventDefault();
+    }
+  }
+}
+window.addEventListener("load", tabs);
 document.querySelectorAll(".project").forEach((project) => {
   const video = project.querySelector(".project__preview");
   project.addEventListener("pointerenter", () => {
@@ -6641,102 +6737,6 @@ class ScrollWatcher {
   }
 }
 document.querySelector("[data-fls-watcher]") ? window.addEventListener("load", () => new ScrollWatcher({})) : null;
-class Parallax {
-  constructor(elements) {
-    if (elements.length) {
-      this.elements = Array.from(elements).map((el) => new Parallax.Each(el, this.options));
-    }
-  }
-  destroyEvents() {
-    this.elements.forEach((el) => {
-      el.destroyEvents();
-    });
-  }
-  setEvents() {
-    this.elements.forEach((el) => {
-      el.setEvents();
-    });
-  }
-}
-Parallax.Each = class {
-  constructor(parent) {
-    this.parent = parent;
-    this.elements = this.parent.querySelectorAll("[data-fls-parallax]");
-    this.animation = this.animationFrame.bind(this);
-    this.offset = 0;
-    this.value = 0;
-    this.smooth = parent.dataset.flsParallaxSmooth ? Number(parent.dataset.flsParallaxSmooth) : 15;
-    this.setEvents();
-  }
-  setEvents() {
-    this.animationID = window.requestAnimationFrame(this.animation);
-  }
-  destroyEvents() {
-    window.cancelAnimationFrame(this.animationID);
-  }
-  animationFrame() {
-    const topToWindow = this.parent.getBoundingClientRect().top;
-    const heightParent = this.parent.offsetHeight;
-    const heightWindow = document.documentElement.clientHeight;
-    const positionParent = {
-      top: topToWindow - heightWindow,
-      bottom: topToWindow + heightParent
-    };
-    const centerPoint = this.parent.dataset.flsParallaxCenter ? this.parent.dataset.flsParallaxCenter : "center";
-    if (positionParent.top < 30 && positionParent.bottom > -30) {
-      switch (centerPoint) {
-        // верхній точці (початок батька стикається верхнього краю екрану)
-        case "top":
-          this.offset = -1 * topToWindow;
-          break;
-        // центрі екрана (середина батька у середині екрана)
-        case "center":
-          this.offset = heightWindow / 2 - (topToWindow + heightParent / 2);
-          break;
-        // Початок: нижня частина екрана = верхня частина батька
-        case "bottom":
-          this.offset = heightWindow - (topToWindow + heightParent);
-          break;
-      }
-    }
-    this.value = this.offset;
-    this.animationID = window.requestAnimationFrame(this.animation);
-    this.elements.forEach((el) => {
-      const parameters = {
-        axis: el.dataset.axis ? el.dataset.axis : "v",
-        direction: el.dataset.flsParallaxDirection ? el.dataset.flsParallaxDirection + "1" : "-1",
-        coefficient: el.dataset.flsParallaxCoefficient ? Number(el.dataset.flsParallaxCoefficient) : 5,
-        min: el.dataset.flsParallaxMin !== void 0 ? Number(el.dataset.flsParallaxMin) : null,
-        max: el.dataset.flsParallaxMax !== void 0 ? Number(el.dataset.flsParallaxMax) : null,
-        additionalProperties: el.dataset.flsParallaxProperties ? el.dataset.flsParallaxProperties : ""
-      };
-      this.parameters(el, parameters);
-    });
-  }
-  parameters(el, parameters) {
-    let translateValue = parameters.direction * (this.value / parameters.coefficient);
-    if (parameters.min !== null) {
-      translateValue = Math.max(parameters.min, translateValue);
-    }
-    if (parameters.max !== null) {
-      translateValue = Math.min(parameters.max, translateValue);
-    }
-    if (parameters.axis === "v") {
-      el.style.transform = `
-			translate3D(0, ${translateValue.toFixed(2)}px, 0)
-			${parameters.additionalProperties}
-		`;
-    } else if (parameters.axis === "h") {
-      el.style.transform = `
-			translate3D(${translateValue.toFixed(2)}px, 0, 0)
-			${parameters.additionalProperties}
-		`;
-    }
-  }
-};
-if (document.querySelector("[data-fls-parallax-parent]")) {
-  new Parallax(document.querySelectorAll("[data-fls-parallax-parent]"));
-}
 class MousePRLX {
   constructor(props, data = null) {
     let defaultConfig = {
